a063852c013837ddf0f73d788dabb9dbbe1c7dfe
Chris4891 <kolar.kristijan84@gmail.com>
2023-04-24 20:27:09 +0200

    add quizes boxes with styles

--- js/app.js
+++
@@ -1,7 +0,0 @@
-// Select all elements with the "tilt-image" class
-const tiltImages = document.querySelectorAll('.quiz-box')
-
-// Initialize Vanilla Tilt for each element
-tiltImages.forEach(image => {
-  VanillaTilt.init(image)
-})
