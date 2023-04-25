fce998b4b1fdb2c1bb245823cd687863f595eb58
Chris4891 <kolar.kristijan84@gmail.com>
2023-04-25 17:31:38 +0200

    add math questions

--- js/app.js
+++ js/app.js
@@ -79,19 +79,55 @@ var score = 0
 
 const mathQuiz = [
   {
-    question: 'What is 2 + 2?',
-    options: ['3', '4', '5', '6'],
-    answer: '4',
+    question: 'What is 5 x 8?',
+    options: ['32', '40', '45', '50'],
+    answer: '40',
   },
   {
-    question: 'What is 10 - 5?',
-    options: ['2', '3', '4', '5'],
-    answer: '5',
+    question: 'What is the value of pi (π) rounded to two decimal places?',
+    options: ['3.14', '3.12', '3.16', '3.18'],
+    answer: '3.14',
   },
   {
-    question: 'What is 8 x 3?',
-    options: ['21', '24', '25', '28'],
-    answer: '24',
+    question: 'What is the square root of 64?',
+    options: ['8', '6', '4', '2'],
+    answer: '8',
+  },
+  {
+    question: 'What is 20% of 75?',
+    options: ['10', '15', '20', '25'],
+    answer: '15',
+  },
+  {
+    question: 'What is the smallest prime number?',
+    options: ['1', '2', '3', '4'],
+    answer: '2',
+  },
+  {
+    question: 'What is the formula for the area of a circle?',
+    options: ['A = l x w', 'A = bh', 'A = πr²', 'A = (b1 + b2)h/2'],
+    answer: 'A = πr²',
+  },
+  {
+    question: 'What is the formula for the volume of a cube?',
+    options: ['V = l x w x h', 'V = πr²h', 'V = b x h', 'V = s³'],
+    answer: 'V = s³',
+  },
+  {
+    question: 'What is the sum of the angles in a triangle?',
+    options: ['90 degrees', '180 degrees', '270 degrees', '360 degrees'],
+    answer: '180 degrees',
+  },
+  {
+    question:
+      'What is the equation of a straight line in slope-intercept form?',
+    options: ['y = mx + c', 'x = my + c', 'y = mx - c', 'x = my - c'],
+    answer: 'y = mx + c',
+  },
+  {
+    question: 'What is the value of 2² + 3³?',
+    options: ['31', '24', '27', '25'],
+    answer: '31',
   },
 ]
 
@@ -108,9 +144,11 @@ function showQuestion() {
     option.addEventListener('click', function () {
       const selectedOption = this.innerHTML
       if (selectedOption === question.answer) {
+        console.log('correct')
         option.classList.add('correct')
         score++
       } else {
+        console.log('incorrect')
         option.classList.add('incorrect')
       }
       for (let j = 0; j < optionsContainer.children.length; j++) {
