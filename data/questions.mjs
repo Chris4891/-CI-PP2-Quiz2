49b55936e35fec1899465fc2ee1f976bd6e35072
Chris4891 <kolar.kristijan84@gmail.com>
2023-04-25 20:05:57 +0200

    refactor questions and move it to other file

---
+++ data/questions.mjs
@@ -0,0 +1,191 @@
+export const mathQuiz = [
+  {
+    question: 'What is 5 x 8?',
+    options: ['32', '40', '45', '50'],
+    answer: '40',
+  },
+  {
+    question: 'What is the value of pi (π) rounded to two decimal places?',
+    options: ['3.14', '3.12', '3.16', '3.18'],
+    answer: '3.14',
+  },
+  {
+    question: 'What is the square root of 64?',
+    options: ['8', '6', '4', '2'],
+    answer: '8',
+  },
+  {
+    question: 'What is 20% of 75?',
+    options: ['10', '15', '20', '25'],
+    answer: '15',
+  },
+  {
+    question: 'What is the smallest prime number?',
+    options: ['1', '2', '3', '4'],
+    answer: '2',
+  },
+  {
+    question: 'What is the formula for the area of a circle?',
+    options: ['A = l x w', 'A = bh', 'A = πr²', 'A = (b1 + b2)h/2'],
+    answer: 'A = πr²',
+  },
+  {
+    question: 'What is the formula for the volume of a cube?',
+    options: ['V = l x w x h', 'V = πr²h', 'V = b x h', 'V = s³'],
+    answer: 'V = s³',
+  },
+  {
+    question: 'What is the sum of the angles in a triangle?',
+    options: ['90 degrees', '180 degrees', '270 degrees', '360 degrees'],
+    answer: '180 degrees',
+  },
+  {
+    question:
+      'What is the equation of a straight line in slope-intercept form?',
+    options: ['y = mx + c', 'x = my + c', 'y = mx - c', 'x = my - c'],
+    answer: 'y = mx + c',
+  },
+  {
+    question: 'What is the value of 2² + 3³?',
+    options: ['31', '24', '27', '25'],
+    answer: '31',
+  },
+]
+
+export const geographyQuiz = [
+  {
+    question: 'What is the capital city of Australia?',
+    options: ['Sydney', 'Melbourne', 'Canberra', 'Perth'],
+    answer: 'Canberra',
+  },
+  {
+    question: 'Which country is both an island and a continent?',
+    options: ['New Zealand', 'Indonesia', 'Madagascar', 'Australia'],
+    answer: 'Australia',
+  },
+  {
+    question: 'Which country is the largest in area in South America?',
+    options: ['Brazil', 'Argentina', 'Peru', 'Colombia'],
+    answer: 'Brazil',
+  },
+  {
+    question: 'Which desert is located in Northern Africa?',
+    options: [
+      'Sahara Desert',
+      'Gobi Desert',
+      'Kalahari Desert',
+      'Arabian Desert',
+    ],
+    answer: 'Sahara Desert',
+  },
+  {
+    question: 'Which country is known as the "Land of the Rising Sun"?',
+    options: ['China', 'Japan', 'Taiwan', 'South Korea'],
+    answer: 'Japan',
+  },
+  {
+    question: 'In which European country would you find the city of Barcelona?',
+    options: ['Italy', 'Portugal', 'France', 'Spain'],
+    answer: 'Spain',
+  },
+  {
+    question: 'What is the highest mountain in Africa?',
+    options: [
+      'Mount Kilimanjaro',
+      'Mount Kenya',
+      'Mount Elgon',
+      'Mount Stanley',
+    ],
+    answer: 'Mount Kilimanjaro',
+  },
+  {
+    question: 'Which river is the longest in South America?',
+    options: ['Amazon River', 'Paraná River', 'Orinoco River', 'Uruguay River'],
+    answer: 'Amazon River',
+  },
+  {
+    question: 'Which country has the longest coastline in the world?',
+    options: ['Canada', 'Australia', 'Russia', 'United States'],
+    answer: 'Canada',
+  },
+  {
+    question: 'Which ocean lies to the east of Africa?',
+    options: [
+      'Indian Ocean',
+      'Atlantic Ocean',
+      'Pacific Ocean',
+      'Arctic Ocean',
+    ],
+    answer: 'Indian Ocean',
+  },
+]
+
+export const sportsQuiz = [
+  {
+    question: 'Which team won the most recent FIFA World Cup in 2018?',
+    options: ['Germany', 'Argentina', 'France', 'Spain'],
+    answer: 'France',
+  },
+  {
+    question:
+      'Who holds the record for the most Olympic gold medals ever won by an athlete?',
+    options: [
+      'Michael Phelps',
+      'Usain Bolt',
+      'Simone Biles',
+      'Larisa Latynina',
+    ],
+    answer: 'Larisa Latynina',
+  },
+  {
+    question: 'Which country won the first ever cricket World Cup in 1975?',
+    options: ['England', 'Australia', 'West Indies', 'India'],
+    answer: 'West Indies',
+  },
+  {
+    question:
+      'What is the highest score you can achieve in a single turn in bowling?',
+    options: ['10', '50', '100', '300'],
+    answer: '300',
+  },
+  {
+    question: 'Who is the all-time leading scorer in the NBA?',
+    options: [
+      'Michael Jordan',
+      'Kareem Abdul-Jabbar',
+      'Kobe Bryant',
+      'LeBron James',
+    ],
+    answer: 'Kareem Abdul-Jabbar',
+  },
+  {
+    question: 'Which country won the most medals at the 2021 Tokyo Olympics?',
+    options: ['United States', 'China', 'Russia', 'Japan'],
+    answer: 'China',
+  },
+  {
+    question: 'Which country won the most recent Rugby World Cup in 2019?',
+    options: ['New Zealand', 'England', 'South Africa', 'Australia'],
+    answer: 'South Africa',
+  },
+  {
+    question: 'In which sport can you score a "try"?',
+    options: ['Soccer', 'Basketball', 'Rugby', 'Tennis'],
+    answer: 'Rugby',
+  },
+  {
+    question: 'Which athlete has won the most Olympic medals of all time?',
+    options: [
+      'Michael Phelps',
+      'Usain Bolt',
+      'Simone Biles',
+      'Larisa Latynina',
+    ],
+    answer: 'Michael Phelps',
+  },
+  {
+    question: 'In what year were the first modern Olympic Games held?',
+    options: ['1886', '1896', '1906', '1916'],
+    answer: '1896',
+  },
+]
