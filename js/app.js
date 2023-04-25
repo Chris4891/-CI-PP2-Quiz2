dec9f216d340bea46520eb09d6a79be1b10a6a2f
Chris4891 <kolar.kristijan84@gmail.com>
2023-04-25 11:33:58 +0200

    choose quiz functionality

--- js/app.js
+++ js/app.js
@@ -1,17 +1,30 @@
 const math = document.getElementById('math')
 const geography = document.getElementById('geography')
 const sports = document.getElementById('sports')
+const userNameElement = document.getElementById('userName')
+const startQuiz = document.getElementById('startQuiz')
+var quizType = ''
+var userName = ''
 
-function handleSelectedClick(clickedElement) {
-  const elements = [math, geography, sports]
-  elements.forEach(element => {
-    if (element === clickedElement) {
-      element.classList.toggle('selected')
+function handleSelectedClick(clickedQuiz) {
+  const quizes = [math, geography, sports]
+  quizes.forEach(quiz => {
+    if (quiz === clickedQuiz) {
+      quiz.classList.toggle('selected')
+      quizType = clickedQuiz.children[0].textContent
     } else {
-      element.classList.remove('selected')
+      quiz.classList.remove('selected')
     }
   })
 }
+
+userNameElement.addEventListener('change', e => {
+  userName = e.target.value
+})
+
+startQuiz.addEventListener('click', e => {
+  console.log(userName, quizType)
+})
 
 math.addEventListener('click', () => handleSelectedClick(math))
 geography.addEventListener('click', () => handleSelectedClick(geography))
