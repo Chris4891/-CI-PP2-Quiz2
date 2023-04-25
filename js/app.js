26ce03f749d178a68781a15d25bffbaa1e6a0078
Chris4891 <kolar.kristijan84@gmail.com>
2023-04-25 15:28:42 +0200

    working on quiz functionality

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@ const toast = document.getElementById('toast')
 const welcome = document.getElementById('welcome')
 const landing = document.getElementById('landing')
 const currentScreen = document.getElementById('currentScreen')
+const questionScreen = document.getElementById('questionScreen')
 
 var quizType = ''
 var userName = ''
@@ -30,7 +31,7 @@ userNameElement.addEventListener('change', e => {
 })
 
 startQuiz.addEventListener('click', e => {
-  console.log(quizType)
+  console.log(quizType, userName)
   if (!userName) {
     toast.textContent = 'Please enter your username'
     toast.classList.remove('hide')
@@ -45,6 +46,7 @@ startQuiz.addEventListener('click', e => {
     }, 2000)
   } else {
     toast.classList.add('hide')
+    questionScreen.classList.remove('hide')
     if (quizType === 'Math') {
       welcome.textContent = `Welcome to the math quiz, ${userName}`
       landing.classList.add('hide')
@@ -64,3 +66,76 @@ startQuiz.addEventListener('click', e => {
 math.addEventListener('click', () => handleSelectedClick(math))
 geography.addEventListener('click', () => handleSelectedClick(geography))
 sports.addEventListener('click', () => handleSelectedClick(sports))
+
+// WORK WITH QUESTIONS
+const quizContainer = document.getElementById('quiz-container')
+const questionContainer = document.getElementById('question-container')
+const optionsContainer = document.getElementById('options-container')
+const submitBtn = document.getElementById('submit-answer-btn')
+const resultContainer = document.getElementById('result-container')
+
+var currentQuestion = 0
+var score = 0
+
+const mathQuiz = [
+  {
+    question: 'What is 2 + 2?',
+    options: ['3', '4', '5', '6'],
+    answer: '4',
+  },
+  {
+    question: 'What is 10 - 5?',
+    options: ['2', '3', '4', '5'],
+    answer: '5',
+  },
+  {
+    question: 'What is 8 x 3?',
+    options: ['21', '24', '25', '28'],
+    answer: '24',
+  },
+]
+
+function showQuestion() {
+  const question = mathQuiz[currentQuestion]
+  const p = document.createElement('p')
+  p.innerHTML = question.question
+  questionContainer.appendChild(p)
+  optionsContainer.innerHTML = ''
+  for (let i = 0; i < question.options.length; i++) {
+    const option = document.createElement('button')
+    option.classList.add('option')
+    option.innerHTML = question.options[i]
+    option.addEventListener('click', function () {
+      const selectedOption = this.innerHTML
+      if (selectedOption === question.answer) {
+        option.classList.add('correct')
+        score++
+      } else {
+        option.classList.add('incorrect')
+      }
+      for (let j = 0; j < optionsContainer.children.length; j++) {
+        optionsContainer.children[j].removeEventListener(
+          'click',
+          arguments.callee
+        )
+      }
+    })
+    optionsContainer.appendChild(option)
+  }
+}
+
+function showResult() {
+  quizContainer.style.display = 'none'
+  resultContainer.innerHTML = `You got ${score} out of ${mathQuiz.length} questions correct!`
+}
+
+showQuestion()
+
+submitBtn.addEventListener('click', function () {
+  if (currentQuestion < mathQuiz.length - 1) {
+    currentQuestion++
+    showQuestion()
+  } else {
+    showResult()
+  }
+})
